b095e4cdc7aa7bfdf0a9293510120cdb07c1e074
Luke <painelukeb@gmail.com>
2024-08-31 21:19:27 +0200

    randomise cards

--- server.js
+++ server.js
@@ -246,8 +246,16 @@ const generateCardArray = (settings, userCount) => {
     currentCardOptions.splice(randomIndex, 1);
     options.push(randomCard);
   }
-
+  options.shuffle();
   return ({options: options, match: includeMatchingPair});
+};
+
+Array.prototype.shuffle = function() {
+  for (let i = this.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [this[i], this[j]] = [this[j], this[i]];
+  }
+  return this;
 };
 
 // Function to generate a unique room ID
